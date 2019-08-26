f3ff9faba4807277301e99c44838afaac8c1db57
xutean <xutean@gmail.com>
2019-08-26 14:42:35 +0800

    use sendgrid instead of gmail

--- gameStand/settings.py
+++ gameStand/settings.py
@@ -154,13 +154,20 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 EMAIL_BACKEND= "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST= "smtp.gmail.com"
-EMAIL_HOST_USER= "xutean@gmail.com"
-EMAIL_HOST_PASSWORD= "j20DAHsu"
+EMAIL_HOST= "smtp.sendgrid.net"
+EMAIL_HOST_USER= "xutean"
+EMAIL_HOST_PASSWORD= "10986@DORTS"
 EMAIL_PORT= 587
 EMAIL_USE_TLS= True
 DEFAULT_FROM_EMAIL= "xutean@gmail.com"
 
+# EMAIL_BACKEND= "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST= "smtp.gmail.com"
+# EMAIL_HOST_USER= "xutean@gmail.com"
+# EMAIL_HOST_PASSWORD= "j20DAHsu"
+# EMAIL_PORT= 587
+# EMAIL_USE_TLS= True
+# DEFAULT_FROM_EMAIL= "xutean@gmail.com"
 
 # EMAIL_USE_TLS = True
 # EMAIL_HOST = 'smtp.gmail.com'
